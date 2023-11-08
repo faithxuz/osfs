4afdb6ba93fba19c9441757e3c1ff3a9dc9fc1a4
km <kidommoc@gmail.com>
2023-11-08 14:56:26 +0800

    update: pass Disk through processing flow

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,27 @@
  * data bitmap: 16 blocks
  */
 
+use std::error::Error;
+use std::sync::Arc;
 use std::net::TcpListener;
 use threadpool::ThreadPool;
 use simdisk::{
-    SdResult,
     init,
     handle,
-    logger::log
+    logger::log,
+    models::Disk,
 };
 
 const PORT: u16 = 7735;
 
 fn main() {
-    init().unwrap();
+    let mut disk = init().unwrap();
     log("Simdisk started...");
-    server().unwrap();
+    server(disk).unwrap();
 }
 
-fn server() -> SdResult<()>  {
+fn server(disk: Disk) -> Result<(), Box<dyn Error>>  {
+    let mut d = Arc::new(disk);
     let listener = TcpListener::bind(format!("127.0.0.1:{PORT}"))?;
     let pool = ThreadPool::new(4);
 
@@ -37,7 +40,7 @@ fn server() -> SdResult<()>  {
             Err(e) => return Err(Box::new(e))
         };
 
-        pool.execute(move || handle(stream))
+        pool.execute(move || handle(d.clone(), stream))
     }
     // for EVERY request, call fn handle in a new thread
     Ok(())
